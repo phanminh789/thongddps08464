3370d6432969df8aa6aa84140ea34ac7a1ea20a0
BlueShootingStar <BlueShootingStar1809@gmail.com>
2019-03-19 07:23:55 +0700

    Update GopY.js

--- javascript/GopY.js
+++ javascript/GopY.js
@@ -1,10 +1,17 @@
 // this function checks for the ID of sender and send comment to server
 function sendComment() {
 	// variables
+	
 	var id = $("#CMND").val();
+	var fullName = $("#HoTen").val();
 	var idPattern = new RegExp("^[0-9]{9,10}$");
 	
 	// program
+	if (fullName == "") {
+		alert("Tên không được trống");
+        return;
+	}
+	
     if (idPattern.test(id)) {
         alert("Gửi góp ý thành công");
         return;
