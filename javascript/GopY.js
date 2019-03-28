7443677978059459f9d1064763db1604071e7016
BlueShootingStar <BlueShootingStar1809@gmail.com>
2019-03-28 21:18:08 +0700

    Update GopY.js

--- javascript/GopY.js
+++ javascript/GopY.js
@@ -1,23 +1,32 @@
 // this function checks for the ID of sender and send comment to server
 function sendComment() {
+		
 	// variables
 	
 	var id = $("#CMND").val();
 	var fullName = $("#HoTen").val();
+	var contentComment = $("#NoiDungGopY").val();
 	var idPattern = new RegExp("^[0-9]{9,10}$");
 	
 	// program
+	// check for emptyness of textfields and patterns
 	if (fullName == "") {
 		alert("Tên không được trống");
-        return;
+		$("#HoTen").focus();
+        	return;
 	}
 	
-    if (idPattern.test(id)) {
-        alert("Gửi góp ý thành công");
-        return;
-    }
+	if (idPattern.test(id) == false) {
+		alert("Số chứng minh không hợp lệ");
+        	return;
+	}
 	
-	alert("Số chứng minh không hợp lệ");
-	return;
+	if (contentComment == "") {
+		alert("Nội dung góp ý không được trống");
+		$("#HoTen").focus();
+        	return;
+	}
+	
+       	alert("Gửi góp ý thành công");
 };
 // end of sendComment
